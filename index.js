b7730d2998cf75866935e0bbb51b406541235958
jxarco <alexroco.30@gmail.com>
2025-02-26 11:49:57 +0100

    1.0.7

--- index.js
+++ index.js
@@ -8,9 +8,34 @@ var Module = {
     totalDependencies: 0
 };
 
-Module.runEngine = function( callback ) {
+async function onEnginePreInitialized() {
 
-    console.warn("Running WGPUEngine!");
+    await Promise.resolve( Module.Engine.getInstance() ).then( result => {
+
+        if ( !result ) {
+            console.error( "Module Instance is null" );
+        }
+
+        Module.Engine.instance = result;
+
+    } ).catch( error => {
+        console.log( error );
+    });
+
+    Promise.resolve( Module.Engine.instance.setWasmModuleInitialized(true) ).then().catch( error => {
+        console.log( error );
+    });
+}
+
+async function onglTFLoaded( ptr )
+{
+    if( Module.Engine.instance.onglTFLoaded )
+    {
+        Module.Engine.instance.onglTFLoaded( ptr );
+    }
+}
+
+Module.runEngine = function( callback ) {
 
     if (!navigator.gpu) {
         const msg = document.createElement("div");
@@ -25,52 +50,42 @@ Module.runEngine = function( callback ) {
         msg.innerText = "Sorry, WebGPU is not supported by your browser.";
         document.body.appendChild(msg);
         console.error(msg.innerText);
-    } else {
-        let canvas = document.createElement('canvas');
-        canvas.className = "emscripten";
-        canvas.id = "canvas";
-        canvas.tabIndex = -1;
-        Object.assign(canvas.style, {
-            position: "fixed",
-            width: "100%",
-            height: "100%",
-            top: "0%",
-            left: "0%",
-            resize: "both"
-        });
-        canvas.addEventListener("oncontextmenu", (e) => { e.preventDefault() });
-        document.body.appendChild(canvas);
-        this.canvas = canvas;
-
-        window.addEventListener('resize', () => {
-            canvas.clientWidth = window.innerWidth;
-            canvas.clientHeight = window.innerHeight;
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-        }, false);
-
-        var script = document.createElement( 'script' );
-        script.id = "main-script";
-        script.src = "node_modules/@upf-gti/jswgpuengine/engine.js";
-        script.async = true;
-        document.getElementsByTagName( 'head' )[ 0 ].appendChild( script );
-
-        window.loadApp = function() {
-            Promise.resolve( Module.Engine.getInstance() ).then( result => {
-        
-                if ( !result ) {
-                    console.error( "Module Instance is null" );
-                }
-            
-                Module.Engine.instance = result;
-
-                callback();
-            
-            } ).catch( error => {
-                console.log( error );
-            });
-        }
+        return;
     }
+
+    console.warn("Running WGPUEngine!");
+
+    let canvas = document.createElement('canvas');
+    canvas.className = "emscripten";
+    canvas.id = "canvas";
+    canvas.tabIndex = -1;
+    Object.assign(canvas.style, {
+        position: "fixed",
+        width: "100%",
+        height: "100%",
+        top: "0%",
+        left: "0%",
+        resize: "both"
+    });
+    canvas.addEventListener("oncontextmenu", (e) => { e.preventDefault() });
+    document.body.appendChild(canvas);
+    this.canvas = canvas;
+
+    window.addEventListener('resize', () => {
+        canvas.clientWidth = window.innerWidth;
+        canvas.clientHeight = window.innerHeight;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }, false);
+
+    var script = document.createElement( 'script' );
+    script.id = "main-script";
+    script.src = "node_modules/@upf-gti/jswgpuengine/engine.js";
+    // script.src = "digital_locations.js";
+    script.async = true;
+    document.getElementsByTagName( 'head' )[ 0 ].appendChild( script );
+
+    window.onEngineInitialized = callback;
 }
 
 Module._fileStore = function( filename, buffer ) {
@@ -81,5 +96,7 @@ Module._fileStore = function( filename, buffer ) {
 }
 
 window.Module = Module;
+window.onEnginePreInitialized = onEnginePreInitialized;
+window.onglTFLoaded = onglTFLoaded;
 
 export { Module };
